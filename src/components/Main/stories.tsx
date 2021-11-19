9f85e006d1b86455984583acd387ea3bb0b90bae
Paulo Lima <prmlimajr@hotmail.com>
2021-11-19 01:27:15 -0300

    Adicionado Storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Main from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'TypeScript, ReactJS, NextJS e Styled Components',
+  },
+} as Meta;
+
+export const Basic: Story = (args) => <Main {...args} />;
